5cbb5536b3e59e5b2d6543e63acd9c385cee8c18
oliviaresende <olivinharesende@hotmail.com>
2024-04-08 17:01:29 -0300

    feat: adding api service

---
+++ src/services/service.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import md5 from 'md5'
+
+const apiPublicKey = import.meta.env.VITE_APP_API_PUBLIC_KEY;
+const apiPrivateKey = import.meta.env.VITE_APP_API_PRIVATE_KEY;
+const apiBaseUrl = import.meta.env.VITE_APP_API_BASE_URL;
+
+const getTs = () => {
+  return new Date().getTime();
+} 
+
+const getHash = (ts: number) => {
+  return md5(ts+apiPrivateKey+apiPublicKey)
+} 
+
+export const getHeroByName= async(name:string) => {
+  const ts = getTs()
+  const hash = getHash(ts)
+   const res = await axios({
+    method: "get",
+    url: `${apiBaseUrl}/characters?ts=${ts}&apikey=${apiPublicKey}&hash=${hash}&name=${name}`,
+  }).catch((err)=>{
+    console.trace(err)
+  }); 
+
+  return res?.data.data.results;
+}
